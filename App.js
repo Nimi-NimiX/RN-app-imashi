fee6d11f6fa1089051bfc4c377e97f376b0a723c
madhaviimashi <madhaviimashi@gmail.com>
2023-10-05 14:39:32 +0530

    styles added

--- App.js
+++ App.js
@@ -1,33 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import BudgetView from './src/views/Budget';
-import Transactions from './src/views/Transactions';
-import Overview from './src/views/Overview';
-import Profile from './src/views/Profile';
-import { GlobalStyles } from './src/constants/styles';
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import BudgetView from "./src/views/Budget";
+import Transactions from "./src/views/Transactions";
+import Overview from "./src/views/Overview";
+import Profile from "./src/views/Profile";
+import { GlobalStyles } from "./src/constants/styles";
+import { Ionicons } from "@expo/vector-icons";
 
-const Stack = createNativeStackNavigator(); 
+const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 function BottomNavBar() {
   return (
-    <Tab.Navigator initialRouteName='Budget'>
-      <Tab.Screen name="Budget" component={BudgetView} />
-      <Tab.Screen name="Transactions" component={Transactions} />
-      <Tab.Screen name="Overview" component={Overview} />
+    <Tab.Navigator
+      initialRouteName="Budget"
+      screenOptions={{
+        headerStyle: { backgroundColor: GlobalStyles.colors.primary.main },
+        headerTintColor: "white",
+        tabBarStyle: { backgroundColor: GlobalStyles.colors.primary.main },
+        tabBarInactiveTintColor: "white",
+        tabBarActiveTintColor: GlobalStyles.colors.other.textP,
+      }}
+    >
+      <Tab.Screen
+        name="Budget"
+        component={BudgetView}
+        options={{
+          title: "Monthly Budget",
+          tabBarLabel: "Budget",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="wallet" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Transactions"
+        component={Transactions}
+        options={{
+          title: "All Transactions",
+          tabBarLabel: "Transactions",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="list" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Overview"
+        component={Overview}
+        options={{
+          title: "Overview",
+          tabBarLabel: "Overview",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="stats-chart" size={size} color={color} />
+          ),
+        }}
+      />
     </Tab.Navigator>
-  )
+  );
 }
 
 export default function App() {
   return (
     <>
-      <StatusBar style='auto' />
+      <StatusBar style="auto" />
       <NavigationContainer>
-        <Stack.Navigator initialRouteName='BottomNavBar'>
-          <Stack.Screen name="BottomNavBar" component={BottomNavBar} options={{ headerShown: false}}/> 
+        <Stack.Navigator initialRouteName="BottomNavBar">
+          <Stack.Screen
+            name="BottomNavBar"
+            component={BottomNavBar}
+            options={{ headerShown: false }}
+          />
           <Stack.Screen name="Profile" component={Profile} />
         </Stack.Navigator>
       </NavigationContainer>
---
+++ src/constants/styles.js
@@ -0,0 +1,20 @@
+export const GlobalStyles = {
+    colors: {
+        primary: {
+            main: '#23d5c3',  // dark-sky-blue
+            light: '#23d5c336', // Light-sky-blue
+        },
+        secondary: {
+            main: '#d52354b3', // dark-pink
+            light: '#ee9f9f3d', // light-pink
+        },
+        other: {
+            text: '#333333',  // Dark Text Color
+            textH: '#00000099',
+            textP: '#17171799',
+            danger: '#ff6666', // Danger/Warning Color,
+            gray: '#f5f5f5',
+            white: '#ffffff',
+        },
+    }
+}
